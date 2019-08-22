8af98c774cd094c3239f8e6fba53adf590bc0339
toniahurst <antonia.a.hurst@gmail.com>
2019-08-22 23:30:57 +0000

    created a test.

---
+++ spec/controllers/grams_controller_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe GramsController, type: :controller do
+  describe "grams#index action" do
+    it "should successfully show the page" do
+    	get :index
+    	expect(response).to have_http_status(:success)
+    end
+  end
+end
