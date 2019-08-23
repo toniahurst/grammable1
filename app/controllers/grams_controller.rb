185e900cd39c0e40f4d36cc6b51fd17b72e736f7
toniahurst <antonia.a.hurst@gmail.com>
2019-08-23 17:24:59 +0000

    L9. Validated garms. Everything working so far.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,8 +8,12 @@ class GramsController < ApplicationController
 	end
 
 	def create
-		@gram = Gram.create(gram_params)
-		redirect_to root_path
+  		@gram = Gram.create(gram_params)
+  		if @gram.valid?
+    		redirect_to root_path
+  		else
+    		render :new, status: :unprocessable_entity
+ 		end
 	end
 
 	private
